e0be3e28202db359eeaf5864c474ab7e9cbceb8e
Lecerof <j.lecerof@gmail.com>
2018-03-01 16:05:59 +0100

    some cleanup and comments

--- extract_patch.m
+++ extract_patch.m
@@ -1,10 +1,13 @@
 function [x, y] = extract_patch(midpoint, radius, img_size)
-    
+% Function for extracting a patch of an image from a midpoint with  a
+% certain radius. If the indexes is out of the bounds of the image, sets
+% the indexes to the boarders.
+
     minX = midpoint(1)-radius;
     maxX = minX + 2*radius;
     minY = midpoint(2)-radius;
     maxY = minY + 2*radius;
-    
+    % Border conditions
     if minX < 1
         minX = 1;
     end
@@ -17,7 +20,7 @@ function [x, y] = extract_patch(midpoint, radius, img_size)
     if maxY > img_size(1)
         maxY = img_size(1);
     end
-    
+    % Generate x and y
     x = minX:maxX;
     y = minY:maxY;
     
