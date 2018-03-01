96220ca3560f93be678b31d566a08c500f5ba546
Lecerof <j.lecerof@gmail.com>
2018-03-01 15:14:24 +0100

    Comments added to the loss function

--- loss_function.m
+++ loss_function.m
@@ -7,28 +7,32 @@ function [cell_count_diff,nbr_of_outliers, residuals] = loss_function(generated_
 
 validation_indexes_size = size(validation_indexes);
 generated_indexes_size = size(generated_indexes);
-% number of 
+% difference between the number of cell elements and guessed cell elements
 cell_count_diff = validation_indexes_size(2) - generated_indexes_size(2);
+
+% initialize variables
 nbr_of_outliers = 0;
 outlier_index = [];
 validation_inlier_pt = [];
 generated_inlier_pt = [];
+
 for i = 1:generated_indexes_size(2)
     
     length_to_val = vecnorm(validation_indexes - generated_indexes(:,i));
-    
+    % find the index in the validation containing an inlier
     inlier_index = find(length_to_val < threshold);
-    
+    % if there is none, it is an outlier
     if isempty(inlier_index)
         nbr_of_outliers = nbr_of_outliers +1;
         outlier_index = [outlier_index, i];
     else
+        % save the inlier
         validation_inlier_pt = [validation_inlier_pt,validation_indexes(:,inlier_index(1))];
         generated_inlier_pt = [generated_inlier_pt, generated_indexes(:,i)];
         
     end
 end
-
+% Calculate the residual on all the inliers
 residuals = sum(vecnorm(validation_inlier_pt - generated_inlier_pt));
 
 end
