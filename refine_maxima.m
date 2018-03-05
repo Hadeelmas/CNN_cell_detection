ae0b172ff5e60bfd68dc31322604be7a9080f9da
ghjalmar <gudmundurhjalmare@gmail.com>
2018-03-05 16:02:32 +0100

    Sub pixel commit

--- refine_maxima.m
+++ refine_maxima.m
@@ -9,15 +9,7 @@ function refined_maxima = refine_maxima(maxima, B)
         maxima_i = maxima(:,i);
         patch = zeros(3,3);
         % The middle point is the actual maxima
-        patch(2,2) = B(maxima_i(2), maxima_i(1));
-        patch(1,1) = B(maxima_i(2)-1, maxima_i(1)-1);
-        patch(1,2) = B(maxima_i(2)-1, maxima_i(1));
-        patch(1,3) = B(maxima_i(2)-1, maxima_i(1)+1);
-        patch(2,1) = B(maxima_i(2), maxima_i(1)-1);
-        patch(2,3) = B(maxima_i(2), maxima_i(1)+1);
-        patch(3,1) = B(maxima_i(2)+1, maxima_i(1)-1);
-        patch(3,2) = B(maxima_i(2)+1, maxima_i(1));
-        patch(3,3) = B(maxima_i(2)+1, maxima_i(1)+1);
+        patch = patch(maxima_i(1)-1:maxima_i(1)+1, maxima_i(2)-1:maxima_i(2)+1);
         [x, y] = subpixel_precision(patch);
         refined_maxima(:,i) = round(maxima_i+[x;y]);
     end
