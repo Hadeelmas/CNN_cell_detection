e431581ce45b97c9d2a7556e930ef91945c44320
Lecerof <j.lecerof@gmail.com>
2018-03-08 14:10:03 +0100

    forgot to add probmap to run_detector

--- run_detector.m
+++ run_detector.m
@@ -3,10 +3,14 @@ function detections = run_detector(img)
     addpath(genpath(fileparts(which('run_detector.m'))));
     net_path = ['my_network.mat'];
     load(net_path)
+    stride = 1;
     
+    probmap = sliding_cnn(net, img, stride);
+    probmap_pos = probmap(:,:,2);
     gaussian_std = 2;
-    maxima = strict_local_maxima(img, 0.5, gaussian_std);
-    refined_maxima = refine_maxima(maxima, img, gaussian_std);
+    
+    maxima = strict_local_maxima(probmap_pos, 0.5, gaussian_std);
+    refined_maxima = refine_maxima(maxima, probmap_pos, gaussian_std);
     x_refmaxima = (refined_maxima(1,:) - 1) * stride + 1;
     y_refmaxima = (refined_maxima(2,:) - 1) * stride + 1;
     detections = [x_refmaxima; y_refmaxima];
