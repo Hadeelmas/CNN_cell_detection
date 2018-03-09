7fc908c397e7891022d887a3f501a1391c84aa57
Lecerof <j.lecerof@gmail.com>
2018-03-09 15:32:14 +0100

    fixed stride in slide_cfn

--- run_detector.m
+++ run_detector.m
@@ -3,11 +3,10 @@ function detections = run_detector(img)
     addpath(genpath(fileparts(which('run_detector.m'))));
     net_path = ['my_network.mat'];
     load(net_path)
-    stride = 4;
-    % fixa striding. Fixa edge positives. Googla neural network size matlab
-    probmap = sliding_cnn(net, img, stride);
+    stride = 4; % From 2 maximum pooling 2x2 
+    probmap = sliding_fcn(net, img);
     probmap_pos = probmap(:,:,2);
-    gaussian_std = 2;
+    gaussian_std = 1;
     
     maxima = strict_local_maxima(probmap_pos, 0.5, gaussian_std);
     refined_maxima = refine_maxima(maxima, probmap_pos, gaussian_std);
