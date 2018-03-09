cbaedbccd90bdc0c9bf573a9fe30e1e59ed61b7b
Lecerof <j.lecerof@gmail.com>
2018-03-09 13:29:42 +0100

    now translate negative direction aswell in  translation function

--- random_translation.m
+++ random_translation.m
@@ -1,7 +1,7 @@
 function translated_img = random_translation(img)
  
     upper_bound = 5;
-    t = randi(upper_bound, 1, 2);
+    t = randi(2*upper_bound, 1, 2)-upper_bound;
     translated_img = imtranslate(img,t);
 
 end
